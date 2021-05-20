74ac6ecce622c8d72ba3a21ede63e5b524562617
Salas4969 <salas.lorenzo4@gmail>
2021-05-20 12:54:58 -0400

    starter code

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var Score = 0;
+var timeLeft;
+localStorage.setItem("queastion 1", "answer");
+localStorage.setItem("queastion 2", "answer");
+localStorage.setItem("queastion 3", "answer");
+localStorage.setItem("queastion 4", "answer");
+localStorage.setItem("queastion 5", "answer");
+
+// function startQuiz() {
+//     console.log("started")
+// }
+document.getElementById("start").onclick= function() { 
+}
